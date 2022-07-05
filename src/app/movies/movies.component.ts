a70777d227cc0ec6e27d61fa0914f686ad804ac3
Dogan <85043826+1990-fatih@users.noreply.github.com>
2022-07-05 13:20:23 +0200

    Lesson Generatin Companent

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,13 @@
+import { TemplateBindingParseResult } from "@angular/compiler";
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'movies', // <>movies</>
+        	    //.movies   <div class="movies"></div> yada # ise id olur //kullanci bunu cagirinca karsisina gelecek kisim
+    template:'Movies'  
+
+
+})
+export class MoviesComponent {
+
+}
